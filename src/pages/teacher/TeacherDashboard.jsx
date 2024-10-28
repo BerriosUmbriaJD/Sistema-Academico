7e5690a5db2b59dc3d4051b76057a314f39b74e0
BerriosUmbriaJD <147112965+BerriosUmbriaJD@users.noreply.github.com>
2024-10-28 00:48:12 -0400

    Add files via upload

--- src/pages/teacher/TeacherDashboard.jsx
+++ src/pages/teacher/TeacherDashboard.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { BookOpen, Home, Calendar, FileText, Bell, User, Plus, Edit, Trash2, Clock } from 'lucide-react';
-import { Link } from "react-router-dom";
-import { NavItem } from '../../components/NavItem';
+import {  Plus, Edit, Trash2 } from 'lucide-react';
+import { Sidebar } from '../../components/Sidebar';
+import { DashboardHeader } from '../../components/DashboardHeader';
 
 export const TeacherDashboard = () => {
   const [activeTab, setActiveTab] = useState('courses');
@@ -79,35 +79,10 @@ export const TeacherDashboard = () => {
 
   return (
     <div className="flex h-screen bg-gray-100">
-      <aside className="w-64 bg-white shadow-md">
-        <div className="p-4">
-          <Link to="/" className="flex items-center space-x-2">
-            <BookOpen className="h-8 w-8 text-blue-600" />
-            <span className="text-xl font-bold text-gray-800">PlanAcadémico</span>
-          </Link>
-        </div>
-        <nav className="mt-8">
-          <NavItem icon={<Home className="mr-3 h-5 w-5" />} label="Inicio" />
-          <NavItem icon={<Calendar className="mr-3 h-5 w-5" />} label="Cursos" active={activeTab === 'courses'} onClick={() => setActiveTab('courses')} />
-          <NavItem icon={<FileText className="mr-3 h-5 w-5" />} label="Tareas" active={activeTab === 'assignments'} onClick={() => setActiveTab('assignments')} />
-          <NavItem icon={<Clock className="mr-3 h-5 w-5" />} label="Exámenes" active={activeTab === 'exams'} onClick={() => setActiveTab('exams')} />
-        </nav>
-      </aside>
+     <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
 
       <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
-        <header className="bg-white shadow">
-          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-            <h1 className="text-3xl font-bold text-gray-900">Panel de Profesor</h1>
-            <div className="flex items-center space-x-4">
-              <button className="p-2 rounded-full hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                <Bell className="h-5 w-5 text-gray-600" />
-              </button>
-              <button className="p-2 rounded-full hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                <User className="h-5 w-5 text-gray-600" />
-              </button>
-            </div>
-          </div>
-        </header>
+        <DashboardHeader title='Panel de profesor' />
 
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <div className="mb-4 border-b border-gray-200">
